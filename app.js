768fba5991d1b0e9b5d9e870e181ea9e2893f2e6
Luis Enrique <rbtluisenriquerbt@gmail.com>
2016-12-30 21:04:46 -0500

    Ricazo Landing - Starter

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express()
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.send('index.html');
+})
+app.listen(3000, function () {
+  console.log('Ricazo')
+})
---
+++ public/js/rscript.js
@@ -0,0 +1,17 @@
+function go2Productos(){
+  $('html,body').animate({
+      scrollTop: $("#productos").offset().top},
+      'slow');
+}
+
+function go2Cultura(){
+  $('html,body').animate({
+      scrollTop: $("#cultura_ricazo").offset().top},
+      'slow');
+}
+
+function go2Caracteristicas(){
+  $('html,body').animate({
+      scrollTop: $("#caracteristicas").offset().top},
+      'slow');
+}
